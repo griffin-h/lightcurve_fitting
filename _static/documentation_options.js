5d39b4b5e79d79f45cc14345f667ff4d8331638d
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-06-25 18:15:23 +0000

    Update docs after building Travis build 5 of griffin-h/lightcurve_fitting
    The docs were built from the branch 'master' against the commit
    1327a61a32ead11958c2ffe43e739e4b81a43cb6.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/griffin-h/lightcurve_fitting/jobs/353666254.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.0.post41+g8dd4728',
+    VERSION: '0.0.0.post42+g1327a61',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

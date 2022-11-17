fix filter extinction calculation

lightcurve_fitting/filters.py
@@ -230,7 +230,7 @@ class Filter:
             self.dfreq = -dfreq
             self.freq_range = (freq_eff.value - freq0, freq1 - freq_eff.value)
 
-            self.R = extinction_law(self.freq_eff, 1.)
+            self.R = fitzpatrick99(np.array([self.wl_eff.to(u.angstrom).value]), 1.)
 
     def synthesize(self, spectrum, *args, z=0., ebv=0., **kwargs):
         """
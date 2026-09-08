9a686e6ba6ed79cf6925e875048292aa3d8f839c
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2026-09-07 17:01:21 -0700

    avoid sphinx error when building docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,7 +69,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Light Curve Fitting'
-copyright = '2023, Griffin Hosseinzadeh'
+copyright = '2026, Griffin Hosseinzadeh'
 author = 'Griffin Hosseinzadeh'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -107,8 +107,6 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-import sphinx_rtd_theme
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

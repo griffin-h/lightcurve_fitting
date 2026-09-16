3b484c168dadce9c956074350d0abb953dbabc11
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2026-09-16 15:02:57 -0700

    suppress autosectionlabel warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -101,6 +101,7 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+suppress_warnings = ['autosectionlabel.*']
 
 # -- Options for HTML output ----------------------------------------------
 
